c52e8ebbf52ced47d6dbe33ba2748a908e67bce3
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-08-21 17:57:35 +0200

    ForumService en BadRequest/RecordNotFound EException gemaakt

---
+++ src/main/java/com/autiwomen/auti_women/services/ForumService.java
@@ -0,0 +1,102 @@
+package com.autiwomen.auti_women.services;
+
+import com.autiwomen.auti_women.dtos.ForumDto;
+import com.autiwomen.auti_women.exceptions.RecordNotFoundException;
+import com.autiwomen.auti_women.models.Forum;
+import com.autiwomen.auti_women.repositories.ForumRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ForumService {
+
+    private final ForumRepository forumRepository;
+
+    public ForumService(ForumRepository forumRepository) {
+        this.forumRepository = forumRepository;
+    }
+
+    public List<ForumDto> getAllForums() {
+        List<Forum> forumList = forumRepository.findAll();
+        List<ForumDto> forumDtoList = new ArrayList<>();
+
+        for (Forum forum : forumList) {
+            ForumDto forumDto = fromForum(forum);
+            forumDtoList.add(forumDto);
+        }
+
+        return forumDtoList;
+    }
+
+    public ForumDto createForum(ForumDto forumDto) {
+        Forum forum = toForum(forumDto);
+        forumRepository.save(forum);
+        return fromForum(forum);
+    }
+
+    public ForumDto getForumById(Long id) {
+        Optional<Forum> forumId = forumRepository.findById(id);
+        if (forumId.isPresent()) {
+            Forum forum = forumId.get();
+            return fromForum(forum);
+        } else {
+            throw new RecordNotFoundException("Er is geen forum gevonden met id: " + id);
+        }
+    }
+
+    public void deleteForum(@RequestParam Long id) {
+        forumRepository.deleteById(id);
+    }
+
+    public ForumDto updateForum(Long id, ForumDto updateForum) {
+        Optional<Forum> forum = forumRepository.findById(id);
+        if (forum.isEmpty()) {
+            throw new RecordNotFoundException("Er is geen forum gevonden met id: " + id);
+        } else {
+            Forum forum1 = forum.get();
+            forum1.setTitle(updateForum.getTitle());
+            forum1.setText(updateForum.getText());
+            Forum forum2 = forumRepository.save(forum1);
+
+            return fromForum(forum2);
+        }
+    }
+
+    public ForumDto fromForum(Forum forum) {
+        var forumDto = new ForumDto();
+        forumDto.id = forum.getId();
+        forumDto.name = forum.getName();
+        forumDto.age = forum.getAge();
+        forumDto.title = forum.getTitle();
+        forumDto.text = forum.getText();
+        forumDto.date = forum.getDate();
+        forumDto.lastReaction = forum.getLastReaction();
+        forumDto.likes = forum.getLikes();
+        forumDto.comments = forum.getComments();
+        forumDto.views = forum.getViews();
+
+        return forumDto;
+    }
+
+    public Forum toForum(ForumDto forumDto) {
+        var forum = new Forum();
+        forum.setId(forumDto.id);
+        forum.setName(forumDto.name);
+        forum.setAge(forumDto.age);
+        forum.setTitle(forumDto.title);
+        forum.setText(forumDto.text);
+        forum.setDate(forumDto.date);
+        forum.setLastReaction(forumDto.lastReaction);
+        forum.setLikes(forumDto.likes);
+        forum.setComments(forumDto.comments);
+        forum.setViews(forumDto.views);
+
+        return forum;
+    }
+
+
+}
