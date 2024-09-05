1b465c12b4ea6473f8a724d01108d7b58ffef330
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-05 15:37:30 +0200

    DTO authentication Request en Response gemaakt

---
+++ src/main/java/com/autiwomen/auti_women/security/dtos/authentication/AuthenticationResponse.java
@@ -0,0 +1,15 @@
+package com.autiwomen.auti_women.security.dtos.authentication;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+}
