273b0d80f6ef808deba2dba31a6cf9730cd24dae
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-18 14:52:06 +0200

    aanpassingen in git tbv pull request

--- Backend-AutiWomen-main/src/main/java/com/autiwomen/auti_women/security/controllers/LoginController.java
+++ Backend-AutiWomen-main/src/main/java/com/autiwomen/auti_women/security/controllers/LoginController.java
@@ -8,12 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.security.Principal;
 
 @CrossOrigin
 @RestController
@@ -53,35 +50,3 @@ public class LoginController {
     }
 
 }
-
-//    private String hashPassword(String password) {
-//        return new BCryptPasswordEncoder().encode(password);
-//    }
-//
-//    @GetMapping(value = "/authenticated")
-//    public ResponseEntity<Object> authenticated(Authentication authentication, Principal principal) {
-//        return ResponseEntity.ok().body(principal);
-//    }
-//
-//    @PostMapping(value = "/login")
-//    public ResponseEntity<?> createAuthenticationToken(@RequestBody LoginRequest loginRequest) throws Exception {
-//
-//        String username = loginRequest.getUsername();
-//        String password = hashPassword(loginRequest.getPassword());
-//
-//        try {
-//            authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(username, password)
-//            );
-//        }
-//        catch (BadCredentialsException ex) {
-//            throw new Exception("Incorrect username or password", ex);
-//        }
-//
-//        final UserDetails userDetails = userDetailsService
-//                .loadUserByUsername(username);
-//
-//        final String jwt = jwtUtl.generateToken(userDetails);
-//
-//        return ResponseEntity.ok(new LoginResponse(jwt));
-//    }
