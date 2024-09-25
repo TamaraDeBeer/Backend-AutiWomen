704413d483be659288e97c8d87f8c157a0e252fe
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-25 16:40:07 +0200

    data.sql aangemaakt om VARCHAR text in Forum en Comment te verhogen

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
+-- Wijzig de kolom 'text' in de 'forums' tabel naar VARCHAR(4000)
 ALTER TABLE forums ALTER COLUMN text TYPE VARCHAR(4000);
+
+-- Wijzig de kolom 'text' in de 'comments' tabel naar VARCHAR(2000)
 ALTER TABLE comments ALTER COLUMN text TYPE VARCHAR(2000);
