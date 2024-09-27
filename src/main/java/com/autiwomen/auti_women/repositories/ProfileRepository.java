abc0068b70f487967528f2e60aa28c81edd34c82
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-09-27 20:27:16 +0200

    methodes geschreven om een profile(BIO) te maken, ophalen, updaten en verwijderen

--- src/main/java/com/autiwomen/auti_women/repositories/ProfileRepository.java
+++ src/main/java/com/autiwomen/auti_women/repositories/ProfileRepository.java
@@ -1,7 +1,11 @@
 package com.autiwomen.auti_women.repositories;
 
 import com.autiwomen.auti_women.models.Profile;
+import com.autiwomen.auti_women.security.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUser(User user);
 }
