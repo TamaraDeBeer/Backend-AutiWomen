2c56868ebe43876b4c763aa9bbb4a701e64590d5
TamaraDeBeer <tamara.debeer@hotmail.com>
2024-11-25 13:27:32 +0100

    klassendiagram aangepast na feedback Novi

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,16 +3,16 @@ DELETE FROM users WHERE username IN ('Tamara', 'Ariel', 'Elsa', 'Jane', 'Belle',
 DELETE FROM authorities WHERE username IN ('Tamara', 'Ariel', 'Elsa', 'Jane', 'Belle', 'Aurora', 'Tiana', 'Sarabi', 'Nala', 'Moana');
 -- Insert users with encrypted passwords
 INSERT INTO users (username, password, email, apikey, enabled, name, gender, dob, autism_diagnoses, autism_diagnoses_year, profile_picture_url) VALUES
-('Tamara', '$2a$10$xhhXDV6.KeReuYn5syvlLue/T2vAuGdByurwlkCKqbD2QzsuMycju', 'Tamara.debeer@hotmail.com', 'random_api_key', true, 'Tamara', 'Female', '1991-07-06', 'Ja', 2020, 'http://localhost:1991/images/Tamara.jpg'),
-('Ariel', '$2a$10$KF3/Gft6GyEinjqCxTuude0DXEEKXgL.CXzu/BtzD4.AOMvmcKL6q', 'Ariel@ariel.nl', 'random_api_key', true, 'Ariel', 'Female', '2004-08-23', 'Ja', 2010, 'http://localhost:1991/images/Ariel.jpg'),
-('Elsa', '$2a$10$2amNQIKp5I1eihy8wQ9a0uYMLBG1g/71ynWSQ2vvfSDC4nlTywNyq', 'Elsa@Elsa.nl', 'random_api_key', true, 'Elsa', 'Female', '2002-06-20', 'Ja', 2018, 'http://localhost:1991/images/Elsa.jpg'),
-('Jane', '$2a$10$.oaK2hzLiExCAXnPivYiN.7F0RN8DOXkiyC/mJ3MvcjKYSoAvkRd.', 'Jane@Jane.nl', 'random_api_key', true, 'Jane', 'Female', '2000-02-05', 'Ja', 2015, 'http://localhost:1991/images/Jane.jpg'),
-('Belle', '$2a$10$Ho5SZodrewE3fhCAO1ybCONs8WT8rmOFQ9WumN.2wEKFDAseBY.lO', 'Belle@belle.nl', 'random_api_key', true, 'Belle', 'Female', '1995-12-06', 'Ja', 2020, 'http://localhost:1991/images/Belle.jpg'),
-('Aurora', '$2a$10$erjisExK6kpY6L7PNAwz2usXIQFWA6/MCmNEH8ICOrD6KMAI33MYC', 'Aurora@aurora.nl', 'random_api_key', true, 'Aurora', 'Female', '1945-12-06', 'Ja', 1963, 'http://localhost:1991/images/Aurora.jpg'),
-('Tiana', '$2a$10$n05DkXn0Ao9PXtkuAU5ygObkOYqhANhZsQcmB3W451ONLOD2Gi55i', 'Tiana@tiana.nl', 'random_api_key', true, 'Tiana', 'Female', '1955-09-15', 'Ja', 1980, 'http://localhost:1991/images/Tiana.jpg'),
-('Sarabi', '$2a$10$RgcGrZDTAFNNroU.MtsBd.kh2/B0B5hvIRSY8K13vSe7BSF/Dgxeq', 'Sarabi@sarabi.nl', 'random_api_key', true, 'Sarabi', 'Female', '1988-02-09', 'Ja', 2019, 'http://localhost:1991/images/Sarabi.jpg'),
-('Nala', '$2a$10$B9w9jigkjejKj6./FZi63OFsCCrLvg/PHi5FPCxHcmBA24QzWUICq', 'Nala@nala.nl', 'random_api_key', true, 'Nala', 'Female', '1993-11-11', 'Ja', 2022, 'http://localhost:1991/images/Nala.jpg'),
-('Moana', '$2a$10$RnnTH/BXaD72WxwV5d3sYe9RJqTtppn16Mexj1SQPKNMa9Ky3FfRi', 'Moana@moana.nl', 'random_api_key', true, 'Moana', 'Female', '1980-05-28', 'Ja', 2000, 'http://localhost:1991/images/Moana.jpg');
+('Tamara', '$2a$10$QC3zESXWN3O/sAU/TOJrieUgXuPqurjoLCv0AnCq2Lhg/Vldpigk2', 'Tamara.debeer@hotmail.com', 'random_api_key', true, 'Tamara', 'Female', '1991-07-06', 'Ja', 2020, 'http://localhost:1991/images/Tamara.jpg'),
+('Ariel', '$2a$10$G3R0T6s/bYrPlwSwWpmmROvFWUqMGj.a274JT5AtvqixpJtoF7zq6', 'Ariel@ariel.nl', 'random_api_key', true, 'Ariel', 'Female', '2004-08-23', 'Ja', 2010, 'http://localhost:1991/images/Ariel.jpg'),
+('Elsa', '$2a$10$fXRjj8Ae9Lxn0avmKGilzuthzA69Btk7aSSUlQ0abhLqe77Ofphf.', 'Elsa@Elsa.nl', 'random_api_key', true, 'Elsa', 'Female', '2002-06-20', 'Ja', 2018, 'http://localhost:1991/images/Elsa.jpg'),
+('Jane', '$2a$10$ClB3DfMXRsosEbaB8mtKDO6nvUzEDrAY5qVuzpjJYT8qfB7zvbFqy', 'Jane@Jane.nl', 'random_api_key', true, 'Jane', 'Female', '2000-02-05', 'Ja', 2015, 'http://localhost:1991/images/Jane.jpg'),
+('Belle', '$2a$10$YbA/XeVOxF2XeFUMB9y4be0JWUwwMOgQnm0CgdX26QN3cF3AYWSZy', 'Belle@belle.nl', 'random_api_key', true, 'Belle', 'Female', '1995-12-06', 'Ja', 2020, 'http://localhost:1991/images/Belle.jpg'),
+('Aurora', '$2a$10$wVQMjzgyMhKpW51lWGeEs.ljAij34P7yzYFUbvQi8N3srMq9AtQ52', 'Aurora@aurora.nl', 'random_api_key', true, 'Aurora', 'Female', '1945-12-06', 'Ja', 1963, 'http://localhost:1991/images/Aurora.jpg'),
+('Tiana', '$2a$10$mKitStszU9xkg9Q2XRaBWuFqKD1AtIcUOk5lm.BJEJlVttHRW52Aa', 'Tiana@tiana.nl', 'random_api_key', true, 'Tiana', 'Female', '1955-09-15', 'Ja', 1980, 'http://localhost:1991/images/Tiana.jpg'),
+('Sarabi', '$2a$10$no5tr5wN5hzuH/T8m0/sBOn5MYQ4oe/0RSz3q0uVTD8x.UDuKzx72', 'Sarabi@sarabi.nl', 'random_api_key', true, 'Sarabi', 'Female', '1988-02-09', 'Ja', 2019, 'http://localhost:1991/images/Sarabi.jpg'),
+('Nala', '$2a$10$lP6t5HLrkdRXZqmq34w3NuZ4oV05mY/nyBC2Ps.7QX3oCUD3XtPoa', 'Nala@nala.nl', 'random_api_key', true, 'Nala', 'Female', '1993-11-11', 'Ja', 2022, 'http://localhost:1991/images/Nala.jpg'),
+('Moana', '$2a$10$ndGITCNCBIh0ozGIB0nVpujI.xWOE65xSC4.gZjCfUXwzPBe6Zfxq', 'Moana@moana.nl', 'random_api_key', true, 'Moana', 'Female', '1980-05-28', 'Ja', 2000, 'http://localhost:1991/images/Moana.jpg');
 -- Insert authorities for users
 INSERT INTO authorities (username, authority) VALUES
 ('Tamara', 'ROLE_ADMIN'),
